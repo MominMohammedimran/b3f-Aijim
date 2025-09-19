52fb1f0ae0a095f1b9e320ba15297096741ad2f3
Mohammedimran <mominmohammedimran02@gmail.com>
2025-09-19 17:03:38 +0530

    edited preloader position

--- src/Preloader.tsx
+++ src/Preloader.tsx
@@ -2,12 +2,11 @@ import { motion } from "framer-motion";
 
 export default function Preloader({ onComplete }: { onComplete: () => void }) {
   const parts = [
-    { id: "down", clip: "inset(0 0 79% 0)", anim: { y: [ -80, 0 ] } }, // top 20%
-    { id: "up", clip: "inset(20% 0 60% 0)", anim: { y: [ 80, 0 ] } },  // 2nd 20%
-    { id: "left", clip: "inset(39% 0 41% 0)", anim: { x: [ -80, 0 ] } }, // 3rd 20%
-    { id: "right", clip: "inset(59% 0 21% 0)", anim: { x: [ 80, 0 ] } }, // 4th 20%
-    { id: "center", clip: "inset(80% 0 0 0)", anim: { scale: [0.8, 1] } }, // last 20%
-  ];
+    { id: "down", clip: "inset(0 0 74% 0)", anim: { y: [ -5, 0 ] } }, // top 20%
+    { id: "up", clip: "inset(18% 0 50% 0)", anim: { y: [ 10, 0 ] } },  // 2nd 20%
+    { id: "left", clip: "inset(32% 0 30% 0)", anim: { x: [ -10, 0 ] } }, // 3rd 20%
+    { id: "right", clip: "inset(54% 0 10% 0)", anim: { x: [ 5, 0 ] } }, // 4th 20%
+    ];
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
